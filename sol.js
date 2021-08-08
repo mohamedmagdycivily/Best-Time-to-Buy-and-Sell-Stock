4f19a3eeeec4b3cd1e25e8a1a43bda66fcc4bb1a
mohamed magdy <mohamedmagdycivily@gmail.com>
2021-08-08 15:02:41 +0200

    solved

---
+++ sol.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    //prices[i] is min or making me profit
+    if (prices[i] < min) min = prices[i];
+    else if (profit < prices[i] - min) profit = prices[i] - min;
+  }
+  return profit;
+};
